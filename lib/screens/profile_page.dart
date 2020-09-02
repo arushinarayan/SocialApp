228140bcb30f6d2ba671c4e30fed8d3293b2b710
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2020-09-02 19:59:36 +0530

    Update profile_page.dart

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fru/services/auth.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import 'login_page.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -9,11 +13,33 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          brightness: Brightness.light,
+          actions: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {
+                  AuthService().signOut();
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Login()));
+                  },
+                  child: Icon(
+                    MdiIcons.logout,
+                    color: Colors.black,
+                    size: 25,
+                  ),
+                )
+            )
+          ],
+        ),
       body: Center(
-        child: Text(
+            child: Text(
             "View Profile",
         ),
-      )
+    )
     );
   }
 }
