b91321c6a1c16f00f6b5659fe8d456c6ba9494d9
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2020-09-02 19:55:15 +0530

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:fru/screens/home_page.dart';
+import 'package:fru/screens/login_page.dart';
 import 'package:fru/services/users.dart';
 
 
 class AuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  handleAuth() {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return Home();
+          } else {
+            return Login();
+          }
+        });
+  }
+
   User _userFromFirebaseuser(FirebaseUser user) {
     return user != null ? User(uid: user.uid) : null;
   }
+
+  Future getCurrentUser() async {
+    return await _auth.currentUser();
+  }
+
 
   Future signInEmailAndPass(String email, String password) async {
     try {
@@ -34,9 +54,20 @@ class AuthService {
   Future signOut() async {
     try {
       return await _auth.signOut();
+
     } catch (e) {
       print(e.toString());
       return null;
     }
+  }
+
+  signIn(AuthCredential authCreds) {
+    FirebaseAuth.instance.signInWithCredential(authCreds);
+
+  }
+
+  signInWithOTP(smsCode, verId) {
+    AuthCredential authCreds = PhoneAuthProvider.getCredential(verificationId: verId, smsCode: smsCode);
+    signIn(authCreds);
   }
 }
