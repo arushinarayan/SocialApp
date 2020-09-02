c1fcf94f23bd42620e9107eb9501a7c2c2f22d98
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2020-09-02 19:55:41 +0530

    Update users.dart

--- lib/services/users.dart
+++ lib/services/users.dart
@@ -1,4 +1,20 @@
 class User {
-  String uid;
-  User({this.uid});
+  final String uid;
+  final String Name;
+  final String email;
+
+  User({this.uid, this.Name, this.email});
+
+  User.fromData(Map<String, dynamic> data)
+      : uid = data['id'],
+        Name = data['Name'],
+        email = data['email'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': uid,
+      'Name': Name,
+      'email': email,
+    };
+  }
 }
