a970418a4d7183e06dcc9cc89b5271423c3a0c27
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2020-09-02 20:03:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fru/screens/login.dart';
+import 'package:fru/services/auth.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Login(),
+      home: AuthService().handleAuth(),
       debugShowCheckedModeBanner: false,
     );
   }
