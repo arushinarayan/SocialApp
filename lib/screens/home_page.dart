b985e17271da48551f549e83bdf33ad56b9b19ba
arushinarayan <67413254+arushinarayan@users.noreply.github.com>
2020-09-02 20:01:11 +0530

    Update home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fru/screens/battle_page.dart';
 import 'package:fru/screens/feed_page.dart';
-import 'package:fru/screens/messages_page.dart';
 import 'package:fru/screens/profile_page.dart';
 import 'package:fru/screens/search_page.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -24,32 +23,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 5.0),
-          child: Text("Hello",style: TextStyle(color: Colors.black), ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        brightness: Brightness.light,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return MessagesPage();
-                  }));
-                },
-                child: Icon(
-                    Icons.message,
-                  color: Colors.black,
-                  size: 25,
-                ),
-              )
-          )
-        ],
-      ),
       body: _children[_currentIndex], // new
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.shifting,
@@ -75,6 +48,7 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+
     );
   }
 }
