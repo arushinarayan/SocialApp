e34295c6fa0a4e81e647b61eeef0761e597dc2b4
Shivangi Chaurasia <56307595+shivangi3001@users.noreply.github.com>
2020-09-05 19:42:27 +0530

    Update battle_page.dart

--- lib/screens/battle_page.dart
+++ lib/screens/battle_page.dart
@@ -8,11 +8,91 @@ class _BattlePageState extends State<BattlePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Start a Battle",
-        ),
-      ),
+      body: Container(
+        width: 400,
+        height: 400,
+        
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("\t\t\t\tOpen Battles", 
+            style: TextStyle(
+            fontSize: 28.0, color: Colors.black,
+            fontWeight: FontWeight.normal,
+            fontStyle: FontStyle.italic
+            
+          ),
+          ),
+        
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff888888),
+                border: Border.all(
+                  color: const Color(0xff888888),
+                    width: 68,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                      shape: BoxShape.rectangle
+              ),
+             ),
+             Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff888888),
+                border: Border.all(
+                  color: const Color(0xff888888),
+                    width: 68,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                      shape: BoxShape.rectangle
+              ),
+             ),
+            ],
+          ),
+          Text("\t\t\t\tCommunity Battles", 
+            style: TextStyle(
+            fontSize: 28.0, color: Colors.black,
+            fontWeight: FontWeight.normal,
+            fontStyle: FontStyle.italic
+            
+          ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff888888),
+                border: Border.all(
+                  color: const Color(0xff888888),
+                    width: 68,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                      shape: BoxShape.rectangle
+              ),
+             ),
+             Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff888888),
+                border: Border.all(
+                  color: const Color(0xff888888),
+                    width: 68,
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                      shape: BoxShape.rectangle
+              ),
+             ),
+            ],
+          ),
+          ],
+        )
+        
+      )
     );
   }
 }
